4849230a9d57a6ed73c3a8b0cf12c3e5b2159aae
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-05-16 15:24:16 -0300

    feat(1) Crie as rotas necessárias para a aplicação

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class PageNotFound extends Component {
+  render() {
+    return (
+      <h1 data-testid="page-not-found">Page Not Found</h1>
+    );
+  }
+}
+
+export default PageNotFound;
---
+++ src/pages/Search.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <h1 data-testid="page-search">Search Page</h1>
+    );
+  }
+}
+
+export default Search;
