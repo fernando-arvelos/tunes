b945747a9ee1d713572936cbd434cd4d810bda4e
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-05-17 15:03:58 -0300

    feat(5) Crie o formulário para pesquisar artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,52 @@
 import { Component } from 'react';
 import Header from '../components/Header';
+import '../css/Search.css';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { artistName } = this.state;
+    const charArtistName = 2;
     return (
-      <div data-testid="page-search">
-        <Header />
-      </div>
+      <>
+        <div data-testid="page-search">
+          <Header />
+        </div>
+        <div>
+          <form>
+            <textarea
+              data-testid="search-artist-input"
+              className="search-artist"
+              placeholder="Nome do Artista"
+              name="artistName"
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              onClick={ this.createNameUser }
+              disabled={ artistName.length < charArtistName }
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
+      </>
     );
   }
 }
