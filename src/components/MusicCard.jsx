a73fb3de59837fb9eaa6ac8b7f53fad061380a1d
Fernando Arvelos <fernandoarvelos1@gmail.com>
2023-05-19 18:02:06 -0300

    feat(8) Crie o mecanismo para adicionar músicas na lista de músicas favoritas

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,16 +1,64 @@
 import { Component } from 'react';
 import { arrayOf, shape, string } from 'prop-types';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import '../css/MusicCard.css';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      checkedTracks: {},
+      loadingTracks: {},
+    };
+  }
+
+  handleCheck = async (music) => {
+    const { checkedTracks } = this.state;
+
+    const isChecked = checkedTracks[music.trackId] || false;
+
+    if (isChecked) {
+      this.setState((prevState) => ({
+        checkedTracks: {
+          ...prevState.checkedTracks,
+          [music.trackId]: false,
+        },
+      }));
+    } else {
+      this.setState((prevState) => ({
+        loadingTracks: {
+          ...prevState.loadingTracks,
+          [music.trackId]: true,
+        },
+      }));
+
+      await addSong(music);
+      this.setState((prevState) => ({
+        checkedTracks: {
+          ...prevState.checkedTracks,
+          [music.trackId]: true,
+        },
+        loadingTracks: {
+          ...prevState.loadingTracks,
+          [music.trackId]: false,
+        },
+      }));
+    }
+  };
+
   render() {
     const { musics } = this.props;
+    const { isChecked, loadingTracks, checkedTracks } = this.state;
     return (
       <div>
         <div>
           {
             musics.map((music, index) => (
-              <>
-                <p key={ index }>{music.trackName}</p>
+              <div key={ index } className="player-music">
+                <p>{music.trackName}</p>
                 <audio
                   data-testid="audio-component"
                   src={ music.previewUrl }
@@ -22,7 +70,43 @@ class MusicCard extends Component {
                   <code>audio</code>
                   .
                 </audio>
-              </>
+
+                <label
+                  htmlFor={ music.trackId }
+                  data-testid={ `checkbox-music-${music.trackId}` }
+                >
+                  <input
+                    type="checkbox"
+                    id={ music.trackId }
+                    name="favorite"
+                    onChange={ () => this.handleCheck(music) }
+                    checked={ isChecked }
+                    // style={ { display: 'none' } }
+                  />
+                  {loadingTracks[music.trackId]
+                    ? <Loading />
+                    : (
+                      <p>
+                        {
+                          checkedTracks[music.trackId]
+                            ? (
+                              <AiFillHeart
+                                size={ 25 }
+                                color="red"
+                              />
+                            )
+
+                            : (
+                              <AiOutlineHeart
+                                size={ 25 }
+                              />
+                            )
+                        }
+                      </p>
+                    )}
+                </label>
+
+              </div>
             ))
           }
         </div>
